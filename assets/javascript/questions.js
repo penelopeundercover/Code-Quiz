84c8049b0a2232158f44508262572804951773dc
penelopeundercover <mjseiwert@outlook.com>
2022-07-09 15:33:33 -0500

    questions update

---
+++ assets/javascript/questions.js
@@ -0,0 +1,32 @@
+//This variable is an array with objects in it. Choices is an array inside of an object.
+var questions = [
+    {
+        questionText: "What does DOM stand for?",
+        choices: ["Don't over-masticate.", "Document Ostrich Model", "Document Object Model", "District of Menses"],
+        answer: "Document Object Model"
+    },
+
+    {
+        questionText: "Which of these is not a datatype?",
+        choices: ["var", "string", "number", "Boolean"],
+        answer: "var"
+    },
+
+    {
+        questionText: "Which of the following mutator methods adds something to the beginning of an array?",
+        choices: ["splice()", "unshift()", "pop()", "shift()"],
+        answer: "unshift()"
+    },
+
+    {
+        questionText: "Which function joins two or more strings together without changing their individual content?",
+        choices: ["concat()", "charAt()", "eval()", "isNaN()"],
+        answer: "concat()"
+    },
+
+    {
+        questionText: "What does the parseInt() function do?",
+        choices: ["Combines two strings", "Revitalizes bland mashed potatoes", "Converts a string into a number", "Nothing"],
+        answer: "Converts a string into a number"
+    }
+]
